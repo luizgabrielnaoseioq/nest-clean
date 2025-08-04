c3782c49436edf10baf89b8042ee7652f8227185
luiz.nazario <luiz.nazario@agpr5.com>
2025-08-04 08:56:33 -0300

    Remove configuração do Swagger e ajuste na criação da aplicação no main.ts; adição de tratamento de erro específico no ZodValidationPipe para validação de dados.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3333);
-  const config = new DocumentBuilder()
-    .setTitle("Forum")
-    .setDescription("Api para fazer perguntas e responder perguntas.")
-    .setVersion("1.0")
-    .addTag("forum")
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, documentFactory);
 }
 bootstrap();
--- src/pipes/zod-validation-pipe.ts
+++ src/pipes/zod-validation-pipe.ts
@@ -3,7 +3,8 @@ import {
   ArgumentMetadata,
   BadRequestException,
 } from "@nestjs/common";
-import { ZodSchema } from "zod";
+import { throwError } from "rxjs";
+import { ZodError, ZodSchema } from "zod";
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -13,7 +14,9 @@ export class ZodValidationPipe implements PipeTransform {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
     } catch (error) {
-      if () {} // 8:13
+      if (error instanceof ZodError) {
+        throw new BadRequestException(error.format())
+      } // 8:13
 
       throw new BadRequestException("Validation failed");
     }
