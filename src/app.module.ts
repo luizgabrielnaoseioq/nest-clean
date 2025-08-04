da5e55dc34f824e7c38ad15f28debcdd0c71ce4f
luiz.nazario <luiz.nazario@agpr5.com>
2025-08-04 14:29:12 -0300

    Adiciona arquivos de configuração de ambiente, chaves públicas e privadas, e implementa o módulo de autenticação com suporte a JWT. Cria controladores para autenticação e gerenciamento de contas, além de integrar validação de ambiente com Zod.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
-import { CreateAccountController } from './controllers/create-account.controller';
-import { GetAllAccounts } from './controllers/get-all-accounts.controller';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from 'src/env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { GetAllAccountsController } from './controllers/get-all-accounts.controller'
 
 @Module({
-  controllers: [CreateAccountController, GetAllAccounts],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [GetAllAccountsController, CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
