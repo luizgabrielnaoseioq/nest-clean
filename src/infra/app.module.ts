87940782fe99c9a4d7111b594096eeb8b6b477f4
luiz.nazario <luiz.nazario@agpr5.com>
2025-08-08 13:34:47 -0300

    feat: adiciona modelos de Comment e Attachment e atualiza relações no Prisma
    - Cria os modelos Comment e Attachment no schema do Prisma
    - Atualiza o modelo User para incluir comentários
    - Adiciona comentários e anexos aos modelos Question e Answer
    - Remove arquivos obsoletos relacionados ao gerenciamento de variáveis de ambiente

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { envSchema } from "../env/env";
+import { envSchema } from "./env/env";
 import { AuthModule } from "./auth/auth.module";
 import { HttpModule } from "./http/http.module";
-import { EnvService } from "../env/env.service";
+import { EnvService } from "./env/env.service";
 
 @Module({
   imports: [
--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -4,8 +4,8 @@ import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "./jwt.strategy";
 import { APP_GUARD } from "@nestjs/core";
 import { JwtAuthGuard } from "./jwt-auth.guard";
-import { EnvService } from "@/env/env.service";
-import { EnvModule } from "@/env/env.module";
+import { EnvService } from "@/infra/env/env.service";
+import { EnvModule } from "@/infra/env/env.module";
 
 @Module({
   imports: [
