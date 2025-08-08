7e642341131afe8b6a03d0ab8c0fd133b989c94c
luiz.nazario <luiz.nazario@agpr5.com>
2025-08-08 13:29:21 -0300

    feat: refatora módulo de configuração e autenticação
    - Adiciona o EnvService para gerenciar variáveis de ambiente no AppModule
    - Remove o arquivo env.ts obsoleto e atualiza a injeção de dependência no JwtModule
    - Modifica o bootstrap para utilizar o EnvService na configuração da porta do servidor
    - Atualiza importações para refletir a nova estrutura de arquivos

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { EnvService } from "../env/env.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3333);
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  });
+
+  const configService = app.get(EnvService);
+  const port = configService.get("PORT");
+
+  await app.listen(port);
 }
-bootstrap();
